70e316c2688f4a098cb7fa02164a398291c48782
Sivakumar-Naguri <sivakumarnaguri@gmail.com>
2025-06-01 19:32:36 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,3 +1,3 @@
 set -e
 docker pull nagurisivakumar/simple-python-flask-app:latest
-docker run -d -p 5000:5000 simple-python-flask-app:latest
+docker run -d -p 5000:5000 nagurisivakumar/simple-python-flask-app
